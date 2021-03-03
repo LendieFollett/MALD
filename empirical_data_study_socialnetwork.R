ddb112537b51f826abd378f5c41c01fa09fb5c30
mstuart2097 <mstuart@iastate.edu>
2021-03-03 14:37:39 -0600

    Update empirical_data_study_socialnetwork.R

--- empirical_data_study_socialnetwork.R
+++ empirical_data_study_socialnetwork.R
@@ -32,6 +32,7 @@ MTCH$Date <- as.Date(rownames(MTCH))
 
 y <- 100*(log(TWTR[-1,c("TWTR.Close")]) - log(TWTR[-nrow(TWTR),c("TWTR.Close")]))
 x <- rep(0,length(y))
+T <- length(y)
 source("run_mcmc.R") 
 #R+B iterations of pgas.R and pgas.cpp updates
 #save results! not sure how to do this... 
@@ -40,6 +41,7 @@ saveRDS(keeps,paste0("keepsTWTR.rds"))
 
 y <- 100*(log(SNAP[-1,c("SNAP.Close")]) - log(SNAP[-nrow(SNAP),c("SNAP.Close")]))
 x <- rep(0,length(y))
+T <- length(y)
 source("run_mcmc.R") 
 #R+B iterations of pgas.R and pgas.cpp updates
 #save results! not sure how to do this... 
@@ -48,6 +50,7 @@ saveRDS(keeps,paste0("keepsSNAP.rds"))
 
 y <- 100*(log(YELP[-1,c("YELP.Close")]) - log(YELP[-nrow(YELP),c("YELP.Close")]))
 x <- rep(0,length(y))
+T <- length(y)
 source("run_mcmc.R") 
 #R+B iterations of pgas.R and pgas.cpp updates
 #save results! not sure how to do this... 
@@ -56,6 +59,7 @@ saveRDS(keeps,paste0("keepsYELP.rds"))
 
 y <- 100*(log(MTCH[-1,c("MTCH.Close")]) - log(MTCH[-nrow(MTCH),c("MTCH.Close")]))
 x <- rep(0,length(y))
+T <- length(y)
 source("run_mcmc.R") 
 #R+B iterations of pgas.R and pgas.cpp updates
 #save results! not sure how to do this... 
