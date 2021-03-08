e5ddaffd1054e22f5c6330513f4997bf9baf918b
LendieFollett <lendie.follett@drake.edu>
2021-03-08 08:04:09 -0600

    Update DIC for 2d model

--- DIC_calculations_2d.R
+++ DIC_calculations_2d.R
@@ -1,3 +1,5 @@
+
+rm(list = ls())
 library(ggplot2)
 library(grid)
 library(gridExtra)
@@ -15,4 +17,53 @@ library(quantmod)
 library(RcppTN)
 
 #DIC7 = -4E(ln(p(y|z,theta))) + 2ln(p(y|z-hat, theta-hat))
+
+# DATA AND MCMC SAMPLES ------------
+
+getSymbols("BTC-USD",from = "2014-09-15",to = "2020-09-30")
+BTC <- as.data.frame(`BTC-USD`)
+BTC$Date <- seq(as.Date("2014-09-17"),as.Date("2020-09-30"),by="days")
+BTC$`BTC-USD.Close`[BTC$Date=="2020-04-17"] <- 7096.18
+getSymbols("^GSPC",from = "2014-09-15",to = "2020-09-30")
+SP500 <- as.data.frame(`GSPC`)
+SP500$Date <- as.Date(rownames(SP500))
+Rsequence <- seq(1,nrow(keepsBTC$v), by = 100)
+S <- merge(BTC,SP500)
+T <- nrow(S) - 1
+S <- merge(BTC,SP500)
+y <- as.matrix(100*(log(S[-1,c("BTC-USD.Close","GSPC.Close")]) - log(S[-nrow(S),c("BTC-USD.Close","GSPC.Close")])))
+#download full MCMC results (obtained from run_mcmc.R with fix = FALSE)
+keepsBTCSP <- readRDS("/Users/000766412/Box Sync/ALD_Codes/keepsBTCSP.rds")
+
+#DIC7 = -4E(ln(p(y|z,theta))) + 2ln(p(y|z-hat, theta-hat))
+partial_likelihood1 <-  function(k, y){ #k for keeps, y for correct vector
+  R <- dim(k$v[complete.cases(k$v),])[1]
+  total = 0
+  for (r in 1:R){
+    total = total + (dnorm(y, 
+                           k$mu[r] + k$J[r,],
+                           sqrt(k$v[r,-1]), log = TRUE) %>%sum)/R #average over R draws
+  }
+  return(total)
+}
+
+#use plug-in posterior means of z, theta
+partial_likelihood2 <-  function(k, y){ #k for keeps, y for correct vector
+  dnorm(y, 
+        mean(k$mu) + apply(k$J,2, mean),
+        sqrt(apply(k$v[,-1], 2, mean)), log = TRUE) %>%sum
+  
+}
+
+
+#E(ln(p(y|z,theta)))
+Elnpy_mid_ztheta <- partial_likelihood1(keepsBTC, y[,1]) + partial_likelihood1(keepsSP, y[,2])
+
+lnpy_mid_zhatthetahat <- partial_likelihood2(keepsBTC, y[,1]) + partial_likelihood2(keepsSP, y[,2])
+
+
+DIC7_1d = -4*Elnpy_mid_ztheta + 2*lnpy_mid_zhatthetahat
+DIC7_1d
+
+
 
