2cd9d96d58a09245ad017ef2cf45b8ca8a2a81b3
LendieFollett <lendie.follett@drake.edu>
2021-06-15 15:56:58 -0500

    Update empirical_data_study_meme.R
    posterior means table

--- empirical_data_study_meme.R
+++ empirical_data_study_meme.R
@@ -142,20 +142,10 @@ doESS <- function(x, total){
   }
 }
 
-domean<- function(x, total){
-  R <- total
-  if(length(dim(x)) == 2){ #if it's a data frame
-    return(apply(x[1:R,], 2, median))
-  }else if (length(dim(x)) > 2){
-    return(apply(x[1:R,,], 2:3, function(x){(median(x))}))
-  }else{
-    return(median(x[1:R]))
-  }
-}
 
 #SVMALD
 lapply(keeps[c(4,6:17)], doESS, total = 20000) %>% str()
-lapply(keepsBTCSP[c(4,6:17)], domean, total = 20000) %>% str()
+
 
 plot(keeps$sigma_c[,1], type = "l")
 plot(keeps$sigma_c[,2], type = "l")
@@ -181,12 +171,53 @@ plot(keepsBTCSP$rhoc[1:total])
 plot(keepsBTCSP$xi_y2eta[1:total])
 
 #use for starting values?
+#starting_values <- lapply(keeps, domean, total = 20000)
+#starting_values %>% str
+#starting_values$delta <- apply(starting_values$delta , 2, round)
+#starting_values$xi_y1 <- starting_values$xi_y1c <- starting_values$J[,1] + rnorm(length(starting_values$xi_y1), 0, .001)
+#starting_values$xi_y2 <- starting_values$xi_y2c <- starting_values$J[,2]+ rnorm(length(starting_values$xi_y1), 0, .001)
+#saveRDS(starting_values,"starting_values_MALD.rds")
 
-starting_values <- lapply(keeps, domean, total = 20000)
-starting_values %>% str
-starting_values$delta <- apply(starting_values$delta , 2, round)
-starting_values$xi_y1 <- starting_values$xi_y1c <- starting_values$J[,1] + rnorm(length(starting_values$xi_y1), 0, .001)
-starting_values$xi_y2 <- starting_values$xi_y2c <- starting_values$J[,2]+ rnorm(length(starting_values$xi_y1), 0, .001)
-saveRDS(starting_values,"starting_values_MALD.rds")
+#################################################### 
+# MEANS  ----------
+####################################################
 
+domean<- function(x, total){
+  R <- total
+  if(length(dim(x)) == 2){ #if it's a data frame
+    return(apply(x[1:R,], 2, median))
+  }else if (length(dim(x)) > 2){
+    return(apply(x[1:R,,], 2:3, function(x){(median(x))}))
+  }else{
+    return(median(x[1:R]))
+  }
+}
+
+
+
+names <- lapply(keeps[c(4,6:17)], domean, total = 10) %>%unlist %>%names
+
+keeps_summary <- array(dim = c(16, length(names)))
+model <- rep(NA, 16)
+data <- rep(NA, 16)
+colnames(keeps_summary) <- names
+
+j = 0
+for (m in c("SVMALD", "SVMVN", "SVLD", "SVIND")){
+  for (i in c("BTC", "DOGE", "AMC", "GMC")){
+    j = j + 1
+    #keeps <- readRDS(paste0("keeps_",m ,"_",i, ".rds"))
+    keeps_summary[j,] <- lapply(keeps[c(4,6:17)], domean, total = 10) %>%unlist
+    model[j] <-m
+    data[j] <- i
+  }
+}
+
+keeps_summary <- keeps_summary %>%as.data.frame()%>%
+  mutate_all(round, digits = 2) %>%
+  mutate(lambda = paste0("(",lambda1,", ", lambda2,", ", lambda3,", ", lambda4,")"))%>%
+  dplyr::select(-c(lambda1, lambda2, lambda3, lambda4))
+keeps_summary$model <- model; keeps_summary$series <- data
+
+keeps_summary[,c(ncol(keeps_summary), ncol(keeps_summary)-1, 1:(ncol(keeps_summary)-2))]
 
