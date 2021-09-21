05debe198f77c023412abcde72cf9b411049856b
mstuart2097 <mstuart@iastate.edu>
2021-09-21 09:22:36 -0500

    Update rho files

--- rho_test.R
+++ rho_test.R
@@ -1,12 +1,50 @@
+rm(list=ls())
 library(tidyverse)
 library(quantmod)
+##### RHO PLOTS FOR LONG TIME SERIES #####
+getSymbols("BTC-USD",from = "2014-09-15",to = "2020-09-30")
+BTC <- as.data.frame(`BTC-USD`)
+BTC$Date <- seq(as.Date("2014-09-17"),as.Date("2020-09-30"),by="days")
+BTC$`BTC-USD.Close`[BTC$Date=="2020-04-17"] <- 7096.18
+getSymbols("^GSPC",from = "2014-09-15",to = "2020-09-30")
+SP500 <- as.data.frame(`GSPC`)
+SP500$Date <- as.Date(rownames(SP500))
+S <- merge(BTC,SP500)
+T <- nrow(S) - 1
+Date <- S$Date
+tmp <- 
+for (m in c("IND","LD","MVN","MALD")){
+  keeps <- readRDS(paste0("keeps_long/keepsBTCSP_",m,".rds"))
+  mu <- apply(keeps$mu,2,mean)
+  theta <- apply(keeps$theta,2,mean)
+  kappa <- 1 - apply(keeps$phi,2,mean)
+  sigmav <- apply(keeps$sigma_v,2,mean)
+  J <- apply(keeps$J,c(2,3),mean)
+  V <- apply(keeps$v,c(2,3),mean)
+  eps_y <- (y - mu[1] - J[,1])/sqrt(V[-(T+1),1])
+  eps_v <- (V[-1,1] - V[-(T+1),1] - kappa[1]*(theta[1] - V[-(T+1),1]))/sqrt(V[-(T+1),1])
+  rho <- eps_y*eps_v
+  tmp$tm <- rho
+  names(tmp)[names(tmp)=="tm"] = m
+}
+p <- tmp %>% gather("Model","Rho",-Date) %>%
+  ggplot() +
+  geom_line(aes(x=Date,y=Rho,colour=Model)) + theme_bw() + theme(text = element_text(size = 20))
+ggsave("rho_t.pdf",p, height = 10, width = 14) 
 
+
+##### RHO PLOTS FOR SHORT TIME SERIES #####
 getSymbols("^GSPC",from = "2020-10-01",to = "2021-06-30")
 SP500 <- as.data.frame(`GSPC`)
 SP500$Date <- as.Date(rownames(SP500))
-getSymbols("BTC-USD",from = "2020-10-01",to = "2021-06-30")
-BTC <- as.data.frame(BTC)
-BTC$Date <- as.Date(rownames(BTC))
+BTC <- read.csv("BTC_USD_2020-07-01_2021-06-30-CoinDesk.csv")
+BTC <- BTC %>% dplyr::select(!Currency)
+names(BTC) <- c("Date","BTC-USD.Close","BTC-USD.Open","BTC-USD.High","BTC-USD.Low")
+BTC$Date <- as.Date(BTC$Date)
+DOGE <- read.csv("DOGE_USD_2020-07-01_2021-06-30-CoinDesk.csv")
+DOGE <- DOGE %>% dplyr::select(!Currency)
+DOGE$Date <- as.Date(DOGE$Date)
+names(DOGE) <- c("Date","DOGE-USD.Close","DOGE-USD.Open","DOGE-USD.High","DOGE-USD.Low")
 getSymbols("AMC",from = "2020-10-01",to = "2021-06-30")
 AMC <- as.data.frame(AMC)
 AMC$Date <- as.Date(rownames(AMC))
@@ -26,38 +64,46 @@ getSymbols("BMY",from = "2020-10-01",to = "2021-06-30")
 BMY <- as.data.frame(BMY)
 BMY$Date <- as.Date(rownames(BMY))
 
-S <- BTC %>% merge(AMC) %>% merge(GME) %>% merge(MRNA) %>% merge(DIS) %>%
+S <- BTC %>% merge(DOGE) %>% merge(AMC) %>% merge(GME) %>% merge(MRNA) %>% merge(DIS) %>%
   merge(BBY) %>% merge(BMY)
 T <- nrow(S) - 1
 
-for (i in c("BTC","AMC","GME","MRNA","DIS","BBY","BMY")){
+for (i in c("BTC","DOGE","AMC","GME","MRNA","DIS","BBY","BMY")){
   if (i == "BTC"){
-    y = log(S$`BTC-USD.close`)[-1] - log(S$`BTC-USD.close`)[-(T+1)]
-  } else if (i == "AMC"){
-    y = log(S$AMC.close)[-1] - log(S$AMC.close)[-(T+1)]
+    y = log(S$`BTC-USD.Close`)[-1] - log(S$`BTC-USD.Close`)[-(T+1)]
+  } else if (i == "DOGE"){
+    y = log(S$DOGE.Close)[-1] - log(S$DOGE.Close)[-(T+1)]
+  }else if (i == "AMC"){
+    y = log(S$AMC.Close)[-1] - log(S$AMC.Close)[-(T+1)]
   } else if (i == "GME"){
-    y = log(S$GME.close)[-1] - log(S$GME.close)[-(T+1)]
+    y = log(S$GME.Close)[-1] - log(S$GME.Close)[-(T+1)]
   } else if (i == "MRNA"){
-    y = log(S$MRNA.close)[-1] - log(S$MRNA.close)[-(T+1)]
+    y = log(S$MRNA.Close)[-1] - log(S$MRNA.Close)[-(T+1)]
   } else if (i == "DIS"){
-    y = log(S$DIS.close)[-1] - log(S$DIS.close)[-(T+1)]
+    y = log(S$DIS.Close)[-1] - log(S$DIS.Close)[-(T+1)]
   } else if (i == "BBY"){
-    y = log(S$BBY.close)[-1] - log(S$BBY.close)[-(T+1)]
+    y = log(S$BBY.Close)[-1] - log(S$BBY.Close)[-(T+1)]
   } else {
-    y = log(S$BMY.close)[-1] - log(S$BMY.close)[-(T+1)]
+    y = log(S$BMY.Close)[-1] - log(S$BMY.Close)[-(T+1)]
   }
+  tmp <- data.frame(Date=S$Date[-1])
   for (m in c("SVIND","SVLD","SVMVN","SVMALD")){
-    keeps <- readRDS(paste0("keeps/keeps_",i,"_",m,".rds"))
+    keeps <- readRDS(paste0("keeps/keeps_",m,"_",i,".rds"))
     mu <- apply(keeps$mu,2,mean)
     theta <- apply(keeps$theta,2,mean)
     kappa <- 1 - apply(keeps$phi,2,mean)
     sigmav <- apply(keeps$sigma_v,2,mean)
-    J <- apply(keeps$J,c(1,3),mean)
-    V <- apply(keeps$V,c(1,3),mean)
-    eps_y <- (y - mu - J)/sqrt(V[-(T+1)])
-    eps_v <- (V[-1] - V[-(T+1)] - kappa*(theta - V[-(T+1)]))/sqrt(V[-(T+1)])
+    J <- apply(keeps$J,c(2,3),mean)
+    V <- apply(keeps$v,c(2,3),mean)
+    eps_y <- (y - mu[1] - J[,1])/sqrt(V[-(T+1),1])
+    eps_v <- (V[-1,1] - V[-(T+1),1] - kappa[1]*(theta[1] - V[-(T+1),1]))/sqrt(V[-(T+1),1])
     rho <- eps_y*eps_v
-    ggplot() + geom_point(aes(x=1:T,y=rho)) + xlab("Time") + ylab("rho")
+    tmp$tm <- rho
+    names(tmp)[names(tmp)=="tm"] = m
   }
+  p <- tmp %>% gather("Model","Rho",-Date) %>%
+    ggplot() +
+    geom_line(aes(x=Date,y=Rho,colour=Model)) + theme_bw() + theme(text = element_text(size = 20))
+  ggsave(paste0(i,"_rho_t.pdf"),p, height = 10, width = 14) 
 }
 
